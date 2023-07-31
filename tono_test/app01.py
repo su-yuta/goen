de5b5a3843683fa132fbd91a6ece70c33f67f802
sai-tono <128287071+sai-tono@users.noreply.github.com>
2023-07-31 19:39:57 +0900

    Add files via upload
    更新0731

--- tono_test/app01.py
+++ tono_test/app01.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from streamlit_folium import st_folium
+from streamlit_folium import folium_static
 import folium
 
 df = pd.read_csv("dummy data.csv")
@@ -13,9 +13,23 @@ st.title('駅どこ執事(仮)')
 st.write("### 安心して住める街をあなたに")
 
 
-station = st.sidebar.text_input("仕事の最寄り駅を入力してください", "駅名")
+station = st.sidebar.selectbox(label="仕事の最寄り駅を入力してください",
+                               options=df[df["category"] == "station"]["name"])
 st.sidebar.number_input("検索したい駅までの時間を入力してください", 0, 120, 0)
 submit = st.sidebar.button("検索")
+
+# データを地図に渡す関数を作成する
+
+
+def AreaMarker(df, m):
+    for index, r in df.iterrows():
+
+        # ピンをおく
+        folium.Marker(
+            location=[r.latitude, r.longitude],
+            popup=r["name"],  # rは非整数なのでilocを用いたデータの特定はできない
+        ).add_to(m)
+
 
 # おすすめを表示
 if submit == True:
@@ -24,4 +38,6 @@ if submit == True:
 
 # 地図を表示
 if submit == True:
-    st.map(df)
+    m = folium.Map(location=[center_lat, center_lon], zoom_start=14)
+    AreaMarker(df, m)  # データを地図渡す
+    folium_static(m)  # 地図情報を表示
